31e032634a6a97587ca905b3c71a8a3476d22a2e
eraviart <emmanuel@raviart.com>
2022-09-12 21:54:46 +0200

    Restore version number in setup.py
    a XXX.X.X version number is not compatible with Poetry.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-France',
-    version = 'XXX.X.X',
+    version = '117.0.3',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
